f891119cd43fe23db8ca66e94562a070cc37e542
271554138@qq.com <271554138@qq.com>
2016-03-30 16:59:02 +0800

    clear

--- mobpex_demo/mobpexSdk/src/com/mobpex/plug/utils/PayChannel.java
+++
@@ -1,8 +0,0 @@
-package com.mobpex.plug.utils;
-
-public final class PayChannel {
-	public static String alipay = "ALIPAY";
-	public static String wx = "WECHAT";
-	public static String mobpex = "YEEPAY";
-	public static String upacp = "UPACP";
-}
